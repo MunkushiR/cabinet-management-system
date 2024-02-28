dc56e6aeac64840fca2ecc6fcd6584f5eef91ac5
MunkushiR <naserianr91@gmail.com>
2024-02-28 11:46:00 +0300

    interdepartmentalmeeting

---
+++ cabinet-management-system/src/Components/InterdepartmentalMeetingsPage.js
@@ -0,0 +1,14 @@
+// InterdepartmentalMeetingsPage.js
+import React from 'react';
+import './styles.css'; // Import CSS for styling
+
+function InterdepartmentalMeetingsPage() {
+    return (
+        <div className="container">
+            <h2>Interdepartmental Meetings Page</h2>
+            {/* Add interdepartmental meetings content here */}
+        </div>
+    );
+}
+
+export default InterdepartmentalMeetingsPage;
