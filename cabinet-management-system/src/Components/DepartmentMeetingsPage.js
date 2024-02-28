5444d4390aaf4f2bcc8c185de6b9565b14e3d48b
MunkushiR <naserianr91@gmail.com>
2024-02-28 11:44:47 +0300

    departmentalmeeting

---
+++ cabinet-management-system/src/Components/DepartmentMeetingsPage.js
@@ -0,0 +1,14 @@
+// DepartmentMeetingsPage.js
+import React from 'react';
+import './styles.css'; // Import CSS for styling
+
+function DepartmentMeetingsPage() {
+    return (
+        <div className="container">
+            <h2>Department Meetings Page</h2>
+            {/* Add department meetings content here */}
+        </div>
+    );
+}
+
+export default DepartmentMeetingsPage;
