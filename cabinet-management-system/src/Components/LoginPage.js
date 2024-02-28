87bd2146abe295c084dec48e38d5d0716b2c55cd
MunkushiR <naserianr91@gmail.com>
2024-02-28 11:31:44 +0300

    done

---
+++ cabinet-management-system/src/Components/LoginPage.js
@@ -0,0 +1,14 @@
+// LoginPage.js
+import React from 'react';
+import './styles.css'; // Import CSS for styling
+
+function LoginPage() {
+    return (
+        <div className="container">
+            <h2>Login Page</h2>
+            {/* Add login form here */}
+        </div>
+    );
+}
+
+export default LoginPage;
