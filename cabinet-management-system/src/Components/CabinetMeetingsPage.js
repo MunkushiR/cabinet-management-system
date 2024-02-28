4e9fe04d86b0142544a19f46279af95eb69df7d3
MunkushiR <naserianr91@gmail.com>
2024-02-28 11:43:35 +0300

    cabinatemeeting

---
+++ cabinet-management-system/src/Components/CabinetMeetingsPage.js
@@ -0,0 +1,14 @@
+// CabinetMeetingsPage.js
+import React from 'react';
+import './styles.css'; // Import CSS for styling
+
+function CabinetMeetingsPage() {
+    return (
+        <div className="container">
+            <h2>Cabinet Meetings Page</h2>
+            {/* Add cabinet meetings content here */}
+        </div>
+    );
+}
+
+export default CabinetMeetingsPage;
