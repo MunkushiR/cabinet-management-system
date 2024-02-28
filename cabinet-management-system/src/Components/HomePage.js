d657aa65bffca5620eff7516867f4481cc73ed4a
MunkushiR <naserianr91@gmail.com>
2024-02-28 11:36:49 +0300

    homepage

---
+++ cabinet-management-system/src/Components/HomePage.js
@@ -0,0 +1,14 @@
+// HomePage.js
+import React from 'react';
+import './styles.css'; // Import CSS for styling
+
+function HomePage() {
+    return (
+        <div className="container">
+            <h2>Home Page</h2>
+            {/* Add home page content here */}
+        </div>
+    );
+}
+
+export default HomePage;
